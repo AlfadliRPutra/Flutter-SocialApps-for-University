eea67c87469a4276d221bc6b2c8e4a898d7ac240
AlfadliRPutra <alfadlipoetra@gmail.com>
2023-11-25 21:46:31 +0700

    Membuat View

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './widgets/verticalview.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,48 +9,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "data/logo-unja.png",
-                height: 30,
-              ),
-              SizedBox(width: 8),
-              Text('UNJAnews'),
-            ],
+          leading: Container(
+            width: 15, // Set the width to 20
+            height: 10, // Set the height to 20
+            child: Image.asset(
+              "data/logo-unja.png",
+            ),
           ),
+          title: Text("UNJAnews"),
           backgroundColor: Colors.orange[200],
           centerTitle: true,
-        ),
-        backgroundColor: Color.fromARGB(255, 216, 214, 214),
-        body: ListView(
-          children: [
-            Container(
-              child: Image.asset(
-                "data/logo-unja.png",
-                height: 50,
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  child: Center(child: Text("Header")),
-                ),
-                Expanded(
-                  child: Container(
-                    child: Text(
-                      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro aliquam corrupti eligendi dicta voluptate voluptas atque possimus earum molestias dignissimos, nisi architecto, temporibus itaque doloribus officiis laborum tenetur illum tempore dolores. Et eum, ipsum accusamus necessitatibus blanditiis, sequi dolores soluta sint nobis cumque dolore eos rerum eius, saepe laudantium optio molestiae fuga consequatur sed asperiores fugit distinctio quisquam voluptas! Porro voluptatibus sunt ratione ea earum quos voluptatem ut quae nulla quas digniti",
-                      // Add additional text as needed
-                    ),
-                  ),
-                ),
-              ],
+          actions: [
+            IconButton(
+              padding: EdgeInsets.all(10),
+              onPressed: () {},
+              icon: Icon(Icons.account_circle),
             )
           ],
         ),
+        backgroundColor: Color.fromARGB(255, 216, 214, 214),
+        body: VerticalView(),
       ),
     );
   }
